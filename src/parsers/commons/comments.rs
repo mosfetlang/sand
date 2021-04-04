487398894bdded2d4c2a326d59eeccc0c493887e
Julio Treviño Páez <julio.tpaez@gmail.com>
2021-04-04 20:08:11 +0200

    Improve Comment parser matching more cases

--- src/parsers/commons/comments.rs
+++ src/parsers/commons/comments.rs
@@ -1,15 +1,14 @@
-use jpar::characters::{
-    read_none_of0, read_text, ucd_inline_whitespace0, UCD_LINE_BREAK_WHITESPACE_CHARS,
-};
+use jpar::characters::{read_none_of0, read_text, UCD_LINE_BREAK_WHITESPACE_CHARS};
 use jpar::combinator::verify;
 use jpar::helpers::map_result;
-use jpar::sequence::{preceded, tuple};
+use jpar::sequence::preceded;
 use jpar::verifiers::interval_verifier;
 use jpar::Span;
 
 use crate::parsers::{ParserInput, ParserNode, ParserResult};
 
 pub static COMMENT_START_TOKEN: &str = "#";
+pub static COMMENT_FORBIDDEN_TOKENS: &str = "[{(+-";
 
 /// A single-line comment.
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -42,7 +41,7 @@ impl<'a> Comment<'a> {
     // GETTERS -----–-----–-----–-----–-----–-----–-----–-----–-----–-----–----
 
     pub fn message(&self) -> &'a str {
-        &self.span_content()[COMMENT_START_TOKEN.len()..].trim()
+        &self.span_content()[COMMENT_START_TOKEN.len()..]
     }
 
     // SETTERS -----–-----–-----–-----–-----–-----–-----–-----–-----–-----–----
@@ -74,12 +73,12 @@ impl<'a> Comment<'a> {
             verify(
                 preceded(
                     read_text(COMMENT_START_TOKEN),
-                    tuple((
-                        ucd_inline_whitespace0,
-                        read_none_of0(interval_verifier(UCD_LINE_BREAK_WHITESPACE_CHARS)),
-                    )),
+                    read_none_of0(interval_verifier(UCD_LINE_BREAK_WHITESPACE_CHARS)),
                 ),
-                |_, (ws, content)| content.is_empty() || ws.len() == 1,
+                |_, content| match content.chars().next() {
+                    Some(char) => !COMMENT_FORBIDDEN_TOKENS.contains(char),
+                    None => true,
+                },
             ),
             |input, _| Comment {
                 span: input.substring_to_current(&init_cursor),
@@ -126,7 +125,7 @@ mod test {
         );
         assert_eq!(
             result.message(),
-            "This is a test",
+            " This is a test  ",
             "[1] The message is incorrect"
         );
 
@@ -143,7 +142,7 @@ mod test {
         );
         assert_eq!(
             result.message(),
-            "This is a test",
+            " This is a test",
             "[2] The message is incorrect"
         );
 
@@ -167,14 +166,31 @@ mod test {
             "#  \t  ",
             "[4] The content is incorrect"
         );
-        assert_eq!(result.message(), "", "[4] The message is incorrect");
+        assert_eq!(result.message(), "  \t  ", "[4] The message is incorrect");
+
+        // Case 5: without space
+        let context = ParserContext::default();
+        let content = "#This is a comment";
+        let mut input = ParserInput::new_with_context_and_error(content, context);
+
+        let result = Comment::parse(&mut input).expect("[5] The parser must succeed");
+        assert_eq!(
+            result.span_content(),
+            "#This is a comment",
+            "[5] The content is incorrect"
+        );
+        assert_eq!(
+            result.message(),
+            "This is a comment",
+            "[5] The message is incorrect"
+        );
     }
 
     #[test]
     fn test_parse_error_not_found() {
         // Case 1: other format
         let context = ParserContext::default();
-        let content = "#[tag]";
+        let content = "identifier";
         let mut input = ParserInput::new_with_context_and_error(content, context);
 
         let result = Comment::parse(&mut input).expect_err("[1] The parser must not succeed");
@@ -187,5 +203,16 @@ mod test {
 
         let result = Comment::parse(&mut input).expect_err("[2] The parser must not succeed");
         assert!(result.is_not_found(), "[2] The error is incorrect");
+
+        // Case 3: start by a forbidden char
+        for char in COMMENT_FORBIDDEN_TOKENS.chars() {
+            let context = ParserContext::default();
+            let content = format!("#{}", char);
+            let mut input = ParserInput::new_with_context_and_error(content.as_str(), context);
+
+            let result = Comment::parse(&mut input)
+                .expect_err(format!("[3.{}] The parser must not succeed", char).as_str());
+            assert!(result.is_not_found(), "[3.{}] The error is incorrect", char);
+        }
     }
 }
